5048485a3cd69283b8932dc16992f6493b68d59b
tomicz <darko_darkblack@yahoo.com>
2022-07-29 22:06:40 +0200

    Added boot state.

---
+++ Assets/Scripts/Systems/StateSystem/States/StateBoot.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace Immersed.Systems.StateSystem.States
+{
+    public class StateBoot : State
+    {
+        public override void StateFixedUpdate()
+        {
+
+        }
+
+        public override void StateLateUpdate()
+        {
+
+        }
+
+        public override void StateUpdate()
+        {
+
+        }
+    }
+}
