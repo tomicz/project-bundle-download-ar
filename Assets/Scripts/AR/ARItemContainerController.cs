195898a07f81c093d6bf439b93bcf321c1037d5a
tomicz <darko_darkblack@yahoo.com>
2022-07-29 21:14:16 +0200

    Added ARItemContainerController, it's a child component to ARContentPlacerController. ItemContainer is responsible for grabbing and removing scene objects. It is supposed to rotate/scale/move and place objects in the scene.

---
+++ Assets/Scripts/AR/ARItemContainerController.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+namespace Immersed.AR
+{
+    public class ARItemContainerController : MonoBehaviour
+    {
+        private Transform _grabbedItem;
+
+        public void GrabItem(Transform transform)
+        {
+            _grabbedItem = transform;
+            _grabbedItem.SetParent(transform);
+        }
+
+        public void RemoveItem()
+        {
+            _grabbedItem.SetParent(null);
+            _grabbedItem = null;
+        }
+
+        public void SetPosition(Vector3 position) => transform.position = position;
+
+        public void SetLocalPosition(Vector3 position) => transform.localPosition = position;
+    }
+}
