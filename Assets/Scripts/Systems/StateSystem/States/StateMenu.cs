16323e61915ac91199e92593d474c2579c83acfa
tomicz <darko_darkblack@yahoo.com>
2022-07-29 22:06:50 +0200

    Added menu state

---
+++ Assets/Scripts/Systems/StateSystem/States/StateMenu.cs
@@ -0,0 +1,20 @@
+namespace Immersed.Systems.StateSystem.States
+{
+    public class StateMenu : State
+    {
+        public override void StateFixedUpdate()
+        {
+
+        }
+
+        public override void StateLateUpdate()
+        {
+
+        }
+
+        public override void StateUpdate()
+        {
+
+        }
+    }
+}
