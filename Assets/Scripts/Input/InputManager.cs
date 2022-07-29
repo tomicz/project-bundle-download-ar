e7b2ebdc4de0b204bb88d35def26ea0437bef8eb
tomicz <darko_darkblack@yahoo.com>
2022-07-29 20:16:58 +0200

    Added a simple Input Manager. Supports only left button click.

---
+++ Assets/Scripts/Input/InputManager.cs
@@ -0,0 +1,15 @@
+using System;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public Action OnButtonPressDownEvent;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            OnButtonPressDownEvent.Invoke();
+        }
+    }
+}
