4261fd26db08543e6711d42ec3867f51f4ef17bb
tomicz <darko_darkblack@yahoo.com>
2022-07-30 17:01:24 +0200

    Added first UIView element ConfirmAction which is a window where a client can assign an action dynamically. It can ask a client if he is sure if he wants to execute a certain action. An example would be he wants to close an application, and yes or no will pop up to make sure.

---
+++ Assets/Scripts/UI/UIViewConfirmAction.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using TMPro;
+using System;
+
+namespace Immersed.UI
+{
+    public class UIViewConfirmAction : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text _description;
+
+        private Action ConfirmAction;
+        private Action CancelAction;
+
+        public void Yes()
+        {
+            ConfirmAction?.Invoke();
+            Disable();
+        }
+
+        public void No()
+        {
+            CancelAction?.Invoke();
+            Disable();
+        }
+
+        public void AddAction(string actionDescription, Action action)
+        {
+            gameObject.SetActive(true);
+            _description.text = actionDescription;
+            ConfirmAction = action;
+        }
+
+        private void Enable() => gameObject.SetActive(true);
+
+        private void Disable() => gameObject.SetActive(false);
+    }
+}
