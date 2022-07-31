e47e24121e6d6bcaf1e0c9eae650e87b1a68186f
tomicz <darko_darkblack@yahoo.com>
2022-07-31 05:20:29 +0200

    Added a horizontal list setter that organizes any type in a horizontal list and displays it.

---
+++ Assets/Scripts/UI/ARUI/ARUIViewHorizontalList.cs
@@ -0,0 +1,24 @@
+using System;
+using Immersed.Data;
+using UnityEngine;
+
+namespace Immersed.UI.ARUI
+{
+    public class ARUIViewHorizontalList : ARUIView
+    {
+        [SerializeField] private UIButtonCategory _listItemPlaceholder;
+
+        public void CreateList(ItemContainerData data, Action<int> clickHandler)
+        {
+            for (int i = 0; i < data.Categories.Length; i++)
+            {
+                var listItem = CreateListItem();
+                listItem.SetCategoryName(data.Categories[i].name);
+                listItem.SetIndex(i);
+                listItem.OnButtonClickedEvent += clickHandler;
+            }
+        }
+
+        private UIButtonCategory CreateListItem() => Instantiate(_listItemPlaceholder, transform);
+    }
+}
