fb23cc0b3f7a8f5e544de475db1672a79de5bc67
tomicz <darko_darkblack@yahoo.com>
2022-07-31 05:21:02 +0200

    Added a footer in the shop view. Footer contains content info - name and price.

---
+++ Assets/Scripts/UI/ARUI/ARUIViewFooter.cs
@@ -0,0 +1,22 @@
+using TMPro;
+using UnityEngine;
+
+namespace Immersed.UI.ARUI
+{
+    public class ARUIViewFooter : ARUIView
+    {
+        [SerializeField] private TMP_Text _itemNameText;
+        [SerializeField] private TMP_Text _priceText;
+
+        public void UpdateData(string itemName, float price)
+        {
+            _itemNameText.text = itemName;
+            _priceText.text = "$" + price;
+        }
+
+        public void Buy()
+        {
+
+        }
+    }
+}
