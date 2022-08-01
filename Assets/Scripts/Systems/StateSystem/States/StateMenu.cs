afe1b05f60968e5bb3b1b07c88bb5f0662f079b4
tomicz <darko_darkblack@yahoo.com>
2022-08-01 15:09:25 +0200

    Menu state is now responsible for listening for changes in Input Manager. If the button is pressed or dragged, the Menu state will listen to that and respond accordingly.

--- Assets/Scripts/Systems/StateSystem/States/StateMenu.cs
+++ Assets/Scripts/Systems/StateSystem/States/StateMenu.cs
@@ -1,7 +1,31 @@
+using Immersed.AR;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
 namespace Immersed.Systems.StateSystem.States
 {
     public class StateMenu : State
     {
+        [Header("Dependencies")]
+        [SerializeField] private ARPointer _arPointer;
+        [SerializeField] private InputManager _inputManager;
+        [SerializeField] private CanvasContainer _canvasContainer;
+
+        [Header("State properties")]
+        [SerializeField] private LayerMask _interactableLayer;
+        [SerializeField] private LayerMask _groundLayer;
+
+        public override void OnEnter()
+        {
+            base.OnEnter();
+            _canvasContainer.Enable();
+            _arPointer.ChangeTarget(_interactableLayer);
+
+            _inputManager.OnPointerDownEvent += HandleOnButtonPressedDownEvent;
+            _inputManager.OnPointerHoldEvent += HandleOnPointerDragEvent;
+            _inputManager.OnPointerUpEvent += HandleOnPointerUpEvent;
+        }
+
         public override void StateFixedUpdate()
         {
 
@@ -15,6 +39,31 @@ namespace Immersed.Systems.StateSystem.States
         public override void StateUpdate()
         {
 
+        }
+
+        public override void OnExit()
+        {
+            base.OnExit();
+
+            _inputManager.OnPointerDownEvent -= HandleOnButtonPressedDownEvent;
+            _inputManager.OnPointerHoldEvent -= HandleOnPointerDragEvent;
+            _inputManager.OnPointerUpEvent -= HandleOnPointerUpEvent;
+        }
+
+        private void HandleOnButtonPressedDownEvent()
+        {
+            _arPointer.RegisterOnPointerSelected();
+        }
+
+        private void HandleOnPointerDragEvent()
+        {
+            _arPointer.ChangeTarget(_groundLayer);
+            _arPointer.RegisterOnPointerDrag();
+        }
+
+        private void HandleOnPointerUpEvent()
+        {
+            _arPointer.ChangeTarget(_interactableLayer);
         }
     }
 }
