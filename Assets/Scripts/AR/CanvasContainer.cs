6ab542f052f7f610f3b8f8d9b36572dab75ce00a
tomicz <darko_darkblack@yahoo.com>
2022-08-01 15:03:06 +0200

    Added a CanvasContainer. It is responsible for containing and managing World Canvas in the scene.

---
+++ Assets/Scripts/AR/CanvasContainer.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+namespace Immersed.AR
+{
+    [RequireComponent(typeof(BoxCollider))]
+    public class CanvasContainer : MonoBehaviour, IARPointerEnter, IARPointerExit, IARPointerDrag
+    {
+        [Header("Dependencies")]
+        [SerializeField] private RectTransform _canvas;
+        [SerializeField] private SpriteRenderer _circleSprite;
+
+        [Header("Properties")]
+        [SerializeField] private Color _onSelectedColor;
+
+        private Color _defaultColor;
+
+        private void Awake()
+        {
+            _defaultColor = _circleSprite.color;
+        }
+
+        public void Enable() => gameObject.SetActive(true);
+
+        public void Disable() => gameObject.SetActive(false);
+
+        public void SetPosition(Vector3 position)
+        {
+            transform.position = position;
+        }
+
+        public void OnPointerEnter()
+        {
+            _circleSprite.color = _onSelectedColor;
+        }
+
+        public void OnPointerExit()
+        {
+            _circleSprite.color = _defaultColor;
+        }
+
+        public void OnPointerDrag(Vector3 position)
+        {
+            transform.position = new Vector3(position.x, transform.position.y, position.z);
+            _canvas.transform.position = new Vector3(position.x, _canvas.transform.position.y, position.z);
+        }
+    }
+}
