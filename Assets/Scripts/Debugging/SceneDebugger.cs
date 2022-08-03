26313779035f85f30c9e8ffd1c44e2b265429f5e
tomicz <darko_darkblack@yahoo.com>
2022-08-03 06:37:23 +0200

    Updated ScenDebugger: Increases debugging speed and simulates AR.

---
+++ Assets/Scripts/Debugging/SceneDebugger.cs
@@ -0,0 +1,28 @@
+using Immersed.AR;
+using UnityEngine;
+
+namespace Immersed.UI
+{
+    public class SceneDebugger : MonoBehaviour
+    {
+        [SerializeField] private ARPointer _arPointer;
+        [SerializeField] private ARContentPlacerController _arContentPlacerController;
+        [SerializeField] private CanvasContainer _canvasContainer;
+        [SerializeField] private Transform _debugARGround;
+
+        private void Awake()
+        {
+#if UNITY_EDITOR
+            _debugARGround.gameObject.SetActive(true);
+#endif
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+      
+            }
+        }
+    }
+}
